918ee7d2cc26b808e07159993a23b9dc713f5fb5
Jarek Decowski <jaroslaw.decowski@payeye.com>
2022-12-04 19:38:38 +0100

    add header component

--- admin-app/src/components/Header.jsx
+++ admin-app/src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = ({ title, subtitle }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
-    return 
-    <Box>
+    return (
+    <Box mb="30px">
         <Typography 
             variant="h2" 
             color={colors.grey[100]} 
@@ -22,4 +22,7 @@ const Header = ({ title, subtitle }) => {
             {subtitle}
         </Typography>
     </Box>
+    )
 }
+
+export default Header;
